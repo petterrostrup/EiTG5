4d551cd101784ca57a66355a836e939afc266877
Lars Olav Skrebergene <konto.gitkraken@skrebergene.no>
2018-02-14 11:03:55 +0100

    Initial project setup
    Imported some textures and Oculus utilities 5

---
+++ EiT/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            position.x += Time.deltaTime * speed;
+        }
+        transform.position = position;
+    }
+}
