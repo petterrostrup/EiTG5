fe470eb0d492268196caec0c278b006e05e31819
Lars Olav Skrebergene <konto.gitkraken@skrebergene.no>
2018-02-21 15:03:58 +0100

    Added new basic scene. Started trying to implement event handlers for the interactive objects, such as the floor.

--- EiT/Assets/_Scripts/VRFloor.cs
+++ EiT/Assets/_Scripts/VRFloor.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRStandardAssets.Utils;
 
 public class VRFloor : MonoBehaviour {
 
-    // Use this for initialization
-    void Start () {
+    [SerializeField] private VRInteractiveItem m_InteractiveItem;
 
+    private void OnEnable()
+    {
+        m_InteractiveItem.OnClick += HandleClick;
+        GameObject player = GameObject.Find("Player");
+        Vector3 playerPos = player.transform.position;
+        playerPos.z += 10;
+        player.transform.position = playerPos;
+    }
+
+
+    private void OnDisable()
+    {
+        m_InteractiveItem.OnClick -= HandleClick;
+    }
+
+    //Handle the Click event
+    private void HandleClick()
+    {
+        Debug.Log("Show click state");
+        GameObject player = GameObject.Find("Player");
+        Vector3 playerPos = player.transform.position;
+        playerPos.y += 20;
+        player.transform.position = playerPos;
+    }
+
+    // Use this for initialization
+    void Start(){
+        GameObject player = GameObject.Find("Player");
+        Vector3 playerPos = player.transform.position;
+        playerPos.y += 20;
+        player.transform.position = playerPos;
     }
 	    
 	// Update is called once per frame
 	void Update () {
-        Vector3 position = transform.position;
-        position.y += 10;
-        transform.position = position;
+        
     }
 }
