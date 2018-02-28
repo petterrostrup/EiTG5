ee0ee1e22ecdf333875d3cd95edd637e04b620d9
Lars Olav Skrebergene <konto.gitkraken@skrebergene.no>
2018-02-28 15:47:19 +0100

    Dynamically updating consumption panel based on the set VRObject properties

--- EiT/Assets/_Scripts/VRObject.cs
+++ EiT/Assets/_Scripts/VRObject.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using VRStandardAssets.Utils;
 
 public class VRObject : MonoBehaviour {
 
     [SerializeField] private VRInteractiveItem m_InteractiveItem;
+    [SerializeField] private GameObject usagePanel;
 
     [SerializeField] private bool usesPower;
     [SerializeField] private bool usesWater;
@@ -36,25 +38,39 @@ public class VRObject : MonoBehaviour {
     //Handle the Over event
     private void HandleOver()
     {
-        GameObject usagePanel = GameObject.Find("Momentanforbruk");
+        usagePanel.SetActive(true);
         Vector3 objectPos = m_InteractiveItem.transform.position;
         Quaternion objectRot = m_InteractiveItem.transform.rotation;
         objectPos.y += 2 + m_InteractiveItem.transform.localScale.y;
 
         usagePanel.transform.position = objectPos;
         usagePanel.transform.rotation = objectRot;
+
+        Transform child = usagePanel.transform.Find("UsageText");
+        Text t = child.GetComponent<Text>();
+        t.text = "";
+
+        if (usesPower)
+        {
+            t.text = t.text + "Power consumption:  " + powerConsumptions[0].ToString() + "W";
+        }
+        if (usesWater)
+        {
+            t.text = t.text + "\nWater consumption: " + waterConsumptions[0].ToString() + "L/h";
+        }
+
     }
 
     //Handle the Out event
     private void HandleOut()
     {
-        
+        usagePanel.SetActive(false);
     }
 
     // Use this for initialization
     void Start () {
-		
-	}
+        usagePanel.gameObject.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
