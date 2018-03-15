e7f4ee440d519acc4a95ac31c6403c3a4331f057
Lars Olav Skrebergene <konto.gitkraken@skrebergene.no>
2018-03-15 16:41:56 +0100

    Implemented object than only have modes on, off and standby.
    Can now view consumption and set mode of relevant objects to On, Off or Standby.

---
+++ EiT/Assets/_Scripts/ConsOnOff.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsOnOff : ConsObj {
+
+    private enum Mode { On, Off }
+    private Mode currentMode = Mode.On;
+    public int[] powerConsArray = new int[2] { 7,0 };
+    public int[] waterConsArray = new int[2] { 0,0 };
+
+    public override void SetMode(int modeIndex)
+    {
+        base.SetType(modeIndex);
+        currentMode = (Mode)modeIndex;
+        SetCurrentPowerCons(powerConsArray[(int)currentMode]);
+    }
+
+    // Setting up ConsPanel
+    public override void MoveConsPanel()
+    {
+        base.MoveConsPanel();
+
+        GameObject UIButtonsTwo = consPanel.GetComponent<ConsPanel>().UIButtonsTwo;
+        UIButtonsTwo.SetActive(true);
+        UIButton[] buttons = UIButtonsTwo.GetComponentsInChildren<UIButton>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponentInChildren<Text>().text = ((Mode)i).ToString();
+            if (i == (int)currentMode)
+            {
+                buttons[i].SetSelected();
+            }
+        }
+        SetupConsPanelCollider();
+    }
+
+    // Event handlers
+    public override void HandleOver()
+    {
+        base.HandleOver();
+    }
+
+    public override void HandleOut()
+    {
+        base.HandleOut();
+    }
+
+    public override void HandleClick()
+    {
+        base.HandleClick();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ EiT/Assets/_Scripts/ConsOnOffStandby.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsOnOffStandby : ConsObj {
+
+    private enum Mode { On, Standby, Off }
+    private Mode currentMode = Mode.On;
+    public int[] powerConsArray = new int[3] { 7, 2, 0 };
+    public int[] waterConsArray = new int[3] { 0, 0, 0 };
+
+    public override void SetMode(int modeIndex)
+    {
+        base.SetType(modeIndex);
+        currentMode = (Mode)modeIndex;
+        SetCurrentPowerCons(powerConsArray[(int)currentMode]);
+    }
+
+    // Setting up ConsPanel
+    public override void MoveConsPanel()
+    {
+        base.MoveConsPanel();
+
+        GameObject UIButtonsThree = consPanel.GetComponent<ConsPanel>().UIButtonsThree;
+        UIButtonsThree.SetActive(true);
+        UIButton[] buttons = UIButtonsThree.GetComponentsInChildren<UIButton>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponentInChildren<Text>().text = ((Mode)i).ToString();
+            if (i == (int)currentMode)
+            {
+                buttons[i].SetSelected();
+            }
+        }
+        SetupConsPanelCollider();
+    }
+
+    // Event handlers
+    public override void HandleOver()
+    {
+        base.HandleOver();
+    }
+
+    public override void HandleOut()
+    {
+        base.HandleOut();
+    }
+
+    public override void HandleClick()
+    {
+        base.HandleClick();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
